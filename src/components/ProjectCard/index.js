88f4e63a542f41da903045738d2865c15a57764b
GCaplan <gabriel.caplan@gmail.com>
2020-09-04 01:02:52 -0400

    card body now displaying images

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,22 +1,35 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./style.css";
 
-function ProjectCard() {
+function ProjectCard(props) {
   return (
-    <div className="card">
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </div>
+    <Container fluid="md">
+      <Row>
+        <Col md4={4}>
+          <div className="card">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                className="img"
+                variant="top"
+                alt={props.name}
+                src={props.image}
+              />
+              <Card.Body>
+                <Card.Title>{props.name}</Card.Title>
+                <Card.Text>{props.summary}</Card.Text>
+                <Button variant="primary">{props.deployedUrl}</Button>
+                <Button variant="primary">{props.githubUrl}</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import ProjectCard from "../components/ProjectCard/index";
+import projects from "../projects.json";
 
-const Projects = () => {
-  return (
-    <>
-      <h1>Projects</h1>
-      <ProjectCard />
-    </>
-  );
-};
+class ProjectPage extends Component {
+  state = { projects };
 
-export default Projects;
+  render() {
+    return (
+      <>
+        <h1>Projects</h1>
+
+        {this.state.projects.map((project) => (
+          <ProjectCard
+            image={project.image}
+            name={project.name}
+            deloyedUrl={project.deloyedUrl}
+            githubUrl={project.githubUrl}
+            summary={project.summary}
+          />
+        ))}
+      </>
+    );
+  }
+}
+
+export default ProjectPage;
